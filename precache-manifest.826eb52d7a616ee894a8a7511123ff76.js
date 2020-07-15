309b40736e7a2fe4f0e0384e719d76cd299c8a8d
bundesl1ga <cameron.d.moreno@gmail.com>
2020-07-15 19:14:25 +0200

    Updates

---
+++ precache-manifest.826eb52d7a616ee894a8a7511123ff76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5655cd0e64bf247ea0090e73a20e8a7",
+    "url": "/wiredcraft-frontend-test/index.html"
+  },
+  {
+    "revision": "3d7dbf5221461a49301c",
+    "url": "/wiredcraft-frontend-test/static/js/2.1d11495e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/wiredcraft-frontend-test/static/js/2.1d11495e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774e1fb9c305c7860f96",
+    "url": "/wiredcraft-frontend-test/static/js/main.b3c511b0.chunk.js"
+  },
+  {
+    "revision": "4eb982a2f6bbf0c823a2",
+    "url": "/wiredcraft-frontend-test/static/js/runtime-main.a16b4003.js"
+  }
+]);
